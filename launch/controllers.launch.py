12eafef1f9a7621d192ee9a966a177e4ec364e73
Tom Wallis <thomaswallis0@gmail.com>
2024-12-24 13:14:14 -0500

    Update for jazy gz

--- launch/controllers.launch.py
+++ launch/controllers.launch.py
@@ -39,19 +39,19 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package='controller_manager',
-        executable='spawner.py',
+        executable='spawner',
         arguments=['diff_controller'],
     )
 
     pid_spawner = Node(
         package='controller_manager',
-        executable='spawner.py',
+        executable='spawner',
         arguments=['pid_controller']
     )
 
     joint_broad_spawner = Node(
         package='controller_manager',
-        executable='spawner.py',
+        executable='spawner',
         arguments=['joint_broad'],
     )
 
